1aa12521fb73b7cd5a49ab101884eb502e7a4528
xclusiveharry <54833048+xclusiveharry@users.noreply.github.com>
2023-01-24 16:53:49 +0100

    fourth commit
    adding category of items to my admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,3 +31,9 @@ Route::middleware([
 });
 
 route::get('/redirect',[HomeController::class, 'redirect']);
+
+route::get('/view_category',[AdminController::class, 'view_category']);
+
+route::post('/add_category',[AdminController::class, 'add_category']);
+
+route::get('/delete_category/{id}',[AdminController::class, 'delete_category']);
